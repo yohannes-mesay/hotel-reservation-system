37d192082e22d120f2aaab873afbe056ec402b44
Yohannes Mesay <125958720+Yohannes-Mesay@users.noreply.github.com>
2024-01-12 23:17:57 +0300

    Update forgotPassword.java

--- src/forgotPassword.java
+++ src/forgotPassword.java
@@ -1,10 +1,7 @@
 
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 
 /**
  *
