08b9a3d40209a42f721314737d489a8b7426b485
yoni-clef <yoniashenafi22@gmail.com>
2024-01-17 11:23:01 +0300

    1 commited

--- src/forgotPassword.java
+++ src/forgotPassword.java
@@ -1,7 +1,10 @@
 
 import javax.swing.JOptionPane;
 
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
 
 /**
  *
