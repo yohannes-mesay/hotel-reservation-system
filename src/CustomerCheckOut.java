37e0880dc1feebd1b0f2d58a7c6387645797906a
yohannaaaa <yohannamerawi@gmail.com>
2024-02-03 22:12:03 +0300

    final

--- src/CustomerCheckOut.java
+++ src/CustomerCheckOut.java
@@ -300,7 +300,7 @@ public class CustomerCheckOut extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-       setVisible(false);
+        setVisible(false);
         new CustomerCheckOut().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
